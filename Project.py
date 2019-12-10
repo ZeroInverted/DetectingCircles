257d8927f3e7cc19a4fbbfef4324fca00a47be5a
Zer0 Inverted <58611801+ZeroInverted@users.noreply.github.com>
2019-12-10 03:49:06 +0200

    16 working extra options

--- Project.py
+++ Project.py
@@ -80,6 +80,239 @@ def checkProgram():
             elif(x in range(856, 860)):
                 return "HAUD"
     return "NONE"
+def check19Option():
+    for(x,y,r) in correctCircles[0, :]:
+        if(y in range(974, 980)): #1.1
+            v11="NONE"
+            if(x in range(1120, 1128)):
+                v11="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v11="Agree"
+            elif(x in range(1322, 1330)):
+                v11="Neutral"
+            elif(x in range(1420, 1428)):
+                v11="Disagree"
+            elif(x in range(1520, 1528)):
+                v11="Strongly Disagree"
+        elif(y in range(1014, 1020)): #1.2
+            v12="NONE"
+            if(x in range(1120, 1128)):
+                v12="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v12="Agree"
+            elif(x in range(1322, 1330)):
+                v12="Neutral"
+            elif(x in range(1420, 1428)):
+                v12="Disagree"
+            elif(x in range(1520, 1528)):
+                v12="Strongly Disagree"
+        elif(y in range(1054, 1062)): #1.3
+            v13="NONE"
+            if(x in range(1120, 1128)):
+                v13="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v13="Agree"
+            elif(x in range(1322, 1330)):
+                v13="Neutral"
+            elif(x in range(1420, 1428)):
+                v13="Disagree"
+            elif(x in range(1520, 1528)):
+                v13="Strongly Disagree"
+        elif(y in range(1094, 1102)): #1.4
+            v14="NONE"
+            if(x in range(1120, 1128)):
+                v14="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v14="Agree"
+            elif(x in range(1322, 1330)):
+                v14="Neutral"
+            elif(x in range(1420, 1428)):
+                v14="Disagree"
+            elif(x in range(1520, 1528)):
+                v14="Strongly Disagree"
+        elif(y in range(1132, 1142)): #1.5
+            v15="NONE"
+            if(x in range(1120, 1128)):
+                v15="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v15="Agree"
+            elif(x in range(1322, 1330)):
+                v15="Neutral"
+            elif(x in range(1420, 1428)):
+                v15="Disagree"
+            elif(x in range(1520, 1528)):
+                v15="Strongly Disagree"
+        elif(y in range(1250, 1266)): #2.1
+            v21="NONE"
+            if(x in range(1120, 1128)):
+                v21="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v21="Agree"
+            elif(x in range(1322, 1330)):
+                v21="Neutral"
+            elif(x in range(1420, 1428)):
+                v21="Disagree"
+            elif(x in range(1520, 1528)):
+                v21="Strongly Disagree"
+        elif(y in range(1290, 1306)): #2.2
+            v22="NONE"
+            if(x in range(1120, 1128)):
+                v22="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v22="Agree"
+            elif(x in range(1322, 1330)):
+                v22="Neutral"
+            elif(x in range(1420, 1428)):
+                v22="Disagree"
+            elif(x in range(1520, 1528)):
+                v22="Strongly Disagree"
+        elif(y in range(1328, 1348)): #2.3
+            v23="NONE"
+            if(x in range(1120, 1128)):
+                v23="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v23="Agree"
+            elif(x in range(1322, 1330)):
+                v23="Neutral"
+            elif(x in range(1420, 1428)):
+                v23="Disagree"
+            elif(x in range(1520, 1528)):
+                v23="Strongly Disagree"
+        elif(y in range(1370, 1386)): #2.4
+            v24="NONE"
+            if(x in range(1120, 1128)):
+                v24="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v24="Agree"
+            elif(x in range(1322, 1330)):
+                v24="Neutral"
+            elif(x in range(1420, 1428)):
+                v24="Disagree"
+            elif(x in range(1520, 1528)):
+                v24="Strongly Disagree"
+        elif(y in range(1408, 1422)): #2.5
+            v25="NONE"
+            if(x in range(1120, 1128)):
+                v25="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v25="Agree"
+            elif(x in range(1322, 1330)):
+                v25="Neutral"
+            elif(x in range(1420, 1428)):
+                v25="Disagree"
+            elif(x in range(1520, 1528)):
+                v25="Strongly Disagree"
+        elif(y in range(1446, 1466)): #2.6
+            v26="NONE"
+            if(x in range(1120, 1128)):
+                v26="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v26="Agree"
+            elif(x in range(1322, 1330)):
+                v26="Neutral"
+            elif(x in range(1420, 1428)):
+                v26="Disagree"
+            elif(x in range(1520, 1528)):
+                v26="Strongly Disagree"
+        elif(y in range(1564, 1584)): #3.1
+            v31="NONE"
+            if(x in range(1120, 1128)):
+                v31="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v31="Agree"
+            elif(x in range(1322, 1330)):
+                v31="Neutral"
+            elif(x in range(1420, 1428)):
+                v31="Disagree"
+            elif(x in range(1520, 1528)):
+                v31="Strongly Disagree"
+        elif(y in range(1604, 1624)): #3.2
+            v32="NONE"
+            if(x in range(1120, 1128)):
+                v32="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v32="Agree"
+            elif(x in range(1322, 1330)):
+                v32="Neutral"
+            elif(x in range(1420, 1428)):
+                v32="Disagree"
+            elif(x in range(1520, 1528)):
+                v32="Strongly Disagree"
+        elif(y in range(1644, 1664)): #3.3
+            v33="NONE"
+            if(x in range(1120, 1128)):
+                v33="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v33="Agree"
+            elif(x in range(1322, 1330)):
+                v33="Neutral"
+            elif(x in range(1420, 1428)):
+                v33="Disagree"
+            elif(x in range(1520, 1528)):
+                v33="Strongly Disagree"
+        elif(y in range(1768, 1788)): #4.1
+            v41="NONE"
+            if(x in range(1120, 1128)):
+                v41="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v41="Agree"
+            elif(x in range(1322, 1330)):
+                v41="Neutral"
+            elif(x in range(1420, 1428)):
+                v41="Disagree"
+            elif(x in range(1520, 1528)):
+                v41="Strongly Disagree"
+        elif(y in range(1808, 1828)): #4.2
+            v42="NONE"
+            if(x in range(1120, 1128)):
+                v42="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v42="Agree"
+            elif(x in range(1322, 1330)):
+                v42="Neutral"
+            elif(x in range(1420, 1428)):
+                v42="Disagree"
+            elif(x in range(1520, 1528)):
+                v42="Strongly Disagree"
+        elif(y in range(1886, 1906)): #4.3
+            v43="NONE"
+            if(x in range(1120, 1128)):
+                v43="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v43="Agree"
+            elif(x in range(1322, 1330)):
+                v43="Neutral"
+            elif(x in range(1420, 1428)):
+                v43="Disagree"
+            elif(x in range(1520, 1528)):
+                v43="Strongly Disagree"
+        elif(y in range(2004, 2024)): #5.1
+            v51="NONE"
+            if(x in range(1120, 1128)):
+                v51="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v51="Agree"
+            elif(x in range(1322, 1330)):
+                v51="Neutral"
+            elif(x in range(1420, 1428)):
+                v51="Disagree"
+            elif(x in range(1520, 1528)):
+                v51="Strongly Disagree"
+        elif(y in range(2046, 2066)): #5.2
+            v52="NONE"
+            if(x in range(1120, 1128)):
+                v52="Strongly Agree"
+            elif(x in range(1218, 1226)):
+                v52="Agree"
+            elif(x in range(1322, 1330)):
+                v52="Neutral"
+            elif(x in range(1420, 1428)):
+                v52="Disagree"
+            elif(x in range(1520, 1528)):
+                v52="Strongly Disagree"
+    print("1.1 Is" + v11 + "\n 1.2 Is "+v12 +"\n1.3 Is " +v13+"\n1.4 is"+v14+ "\n1.5 is "+v15 +"\n2.1 is"+v21+"\n2.2 is"+v22 +"\n2.3 is"+v23)
+    print("2.4 is"+v24+"\n2.5 is"+v25+"\n2.6 is"+v26 + "\n3.1 is "+v31 +"\n3.2 is"+v32 + "\n3.3 is "+v33+"\n4.1 is"+v41+"\n4.2 is"+v42+"\n5.1 is "+v51+"\n5.2 is"+v52)
+
 print("Identified Gender is:"+ checkGender())
 print("Identified Semester is:"+checkSemester())
 print("Identified Credit Hours Program is:"+ checkProgram())
